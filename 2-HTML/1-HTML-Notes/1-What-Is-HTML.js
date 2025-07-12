4654e790739b469bd7616df4bc1d187be3db7f87
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-07-12 21:34:43 +0530

    HTML Basics

---
+++ 2-HTML/1-HTML-Notes/1-What-Is-HTML.js
@@ -0,0 +1,69 @@
+/* 
+--------------------------------------- HTML --------------------------------------
+
+--> What is HTML ?
+    HTML stands for HyperText Markup Language.It is used to give structure to the
+    web page.It is the basic building block of a web page.It is the skeleton of a 
+    web page. 
+
+--> What is a Markup Language ? 
+    It is a markup language.A markup language is not a programming language but 
+    provides some syntax that is used to arrange and structure the content of a 
+    text file. 
+    HTML file is also a text file only but it is a markup text file. 
+
+    Syntax : <openingTag>Content</closingTag>
+
+--> What is HTML Element ? 
+    It is the content that is wrapped inside the opening and closing tag. 
+
+    Ex : <p>This is a text</p>
+
+--> What is a Web Page ? 
+    The combination of HTML , CSS and JavaScript files is called as web page. 
+    Any single html document is called as web page.
+
+--> What is website ? 
+    The combination of web pages is called as web pages.
+
+--> What is HyperText ? 
+    It is a text which contains links to other text.It is a text that contains links,
+    when we click on this link than we redirect to a new web page.
+
+--> What is index.html ? 
+    It is the first file that the server sends to the client as a response and 
+    client renders it. 
+    It is the home page or root file of any website.
+    It is must to have index.html file in each and every website.
+
+--> How to run ?  : To run the HTML file , open the index.html file in any browser.
+
+--> Why HTML was created and who created it ? 
+    HTML was created by Tim Berners-Lee to implement the hypertext feature 
+    in the research papers so that any one can view the references on a new 
+    page.
+
+--> What are Comments ? 
+    They are those parts of the code that is not executed by the browser.
+    HTML Comments : <!-- --> 
+
+--> What is W3C ? 
+    It stands for World Wide Web Consortium.It makes standards for the web.
+
+--> We don't need to install HTML.Browser understands HTML , CSS and JavaScript.
+
+--> What is HTML5 ? 
+    HTML5 is HTML only but it is the latest version of HTML.It contains new 
+    features.
+    The doctype declaration helps us to identify different documents.
+
+--> What is XHTML ? 
+    It stands for extensible HTML.It is strict HTML.It is not used that much.
+
+--> The extension of html is .html
+--> Live server reloads only HTML file.
+--> A text file is also a document.Browser assumes a text file as a document. 
+--> html is responsive by default.
+--> A single html file or other file can make multiple requests.
+
+*/
