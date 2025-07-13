eb98c410dce43e781485e9b68f086ebb31f20547
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-07-13 21:20:03 +0530

    Inline Vs Block Elements And HTML Entities

---
+++ 2-HTML/1-HTML-Notes/4-Inline-Vs-Block-And-HTML-Entities.js
@@ -0,0 +1,39 @@
+/* 
+---------------------- Inline Vs Block Elements And HTML Entities -------------------
+
+--> Inline Vs Block Elements 
+
+    Block Elements : These are those elements that takes the whole single line.They 
+    takes 100% width of their parent elements. 
+
+    Ex : heading elements (h1 to h6) , p , pre , div , footer , main , section , 
+    hgroup , blockquote , figure etc.
+
+    Inline Elements : These are those elements that takes only the required width 
+    according to their content.They don't take 100% width of their parent element. 
+
+    Ex : span , q , u , s , del , ins , abbr , dfn , sup , sub , var , mark , kbd,
+    code , samp etc. 
+
+--> What are HTML entities ? 
+    HTML entities is a way to add special characters/symbols in our HTML document. 
+    They start with & symbol.
+
+    <pre>
+        &quot;
+        &apos; 
+        &copy; 
+        &pound; 
+        &dollar; 
+        &rarr; 
+        &larr; 
+        &uarr; 
+        &darr; 
+        &reg; 
+        &trade; 
+        &star; 
+        &starf; 
+        &euro; 
+    </pre>
+
+*/
