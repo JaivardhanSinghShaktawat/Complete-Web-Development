c6a7a00fb2c9a790102dc6c4e031532c0ad82fc8
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-07-13 20:19:13 +0530

    Structure Of HTML And Meta Elements

---
+++ 2-HTML/1-HTML-Notes/2-Structure-Of-HTML.js
@@ -0,0 +1,47 @@
+/* 
+------------------------------------ Structure Of HTML -----------------------------
+
+--> <!DOCTYPE html> : It is document type declaration.It tells the browser that we 
+    are using HTML document and HTML5 version.
+    Browser understands different types of documents and HTML also has different versions.
+    Each html version has it's own different document type declaration. 
+
+--> html : it is the root element of the html document.It contains two child i.e 
+    head and body. 
+
+--> head : It is the first child of html element.It provides information and meta data
+    about the web page to the search engine.It contains content like meta elements , 
+    title tag , scripts and styles that are not visible on the web page or browser.
+
+--> body : it is the second child of html element.It contains all the content that 
+    will be displayed on the browser. 
+
+--> title : it represents the title of the web page.It can be seen on the tab of the
+    web page.
+
+--> title and favicon are the only thing present in head element that will be 
+    displayed on the web page.
+
+--> favicon : it is the icon that is displayed on the tab side.
+
+--> link element : it is used to import the styles and external resources. 
+    <link rel = "stylesheet" href = "style.css" type = "text/css">
+    Here we are importing stylesheet. 
+
+--> lang = "en" : it is an attribute which means language.It is a global attribute
+    and here we have the set the language to english.
+
+--> charset : it stands for character set.A character set is a collection of 
+    characters.
+    Ex : ASCII , utf-8 , utf-16 etc.
+
+--> Boiler plate code means repetitive code.
+    A server can serve audio , videos , pdf and other files as well.
+
+--> Meta Elements : They provide meta data or meta information about the web page
+    to the web services or search engines like google and other browsers.This 
+    helps them to know about our website and it improves our ranking. 
+    Metadata means data about data. 
+    They improve SEO. 
+
+*/
