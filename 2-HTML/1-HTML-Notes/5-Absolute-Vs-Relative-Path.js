9b68b3ca7dee1ede64bc665a3ca41260a0bc217d
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-07-14 23:27:03 +0530

    Absolute Vs Relative Path Updated

--- 2-HTML/1-HTML-Notes/5-Absolute-Vs-Relative-Path.js
+++ 2-HTML/1-HTML-Notes/5-Absolute-Vs-Relative-Path.js
@@ -4,19 +4,19 @@
 --> Absolute Path : It is the complete path from the root till the file or directory
     that you want. 
 
-    Relative Path : It is the shortest path.It is relative to the current file or 
-    folder.
-
---> Note : Always use relative path while working HTML , CSS and JavaScript.This is 
-    because the website when hosted is deployed on the server.On the server the 
-    absolute path will be different but the relative path won't. 
-
-    Both absolute path and relative path have advantages. 
-
     Absolute path can be used to link external server web pages or external 
     images or other resources.
+    
+--> Relative Path : It is the shortest path.It is relative to the current file or 
+    folder.
 
     Relative path is generally used when we want to provide the path to some 
     resource that is present on the same server. 
+
+--> Note : Always use relative path while working with HTML , CSS and JavaScript.This is 
+    because the website when hosted is deployed on the server.On the server the 
+    absolute path will be different but the relative path won't. 
+    
+    Both absolute path and relative path have advantages. 
 
 */
