7c378dc357206a404665083a85f9e82f538ada60
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-07-14 23:30:22 +0530

    Id Vs Classes

---
+++ 2-HTML/1-HTML-Notes/6-Id-Vs-Classes.js
@@ -0,0 +1,29 @@
+/* 
+----------------------------------- Id Vs Classes ----------------------------------
+
+--> What is Id ? 
+    It stands for unique identifier.It is used to identify an html element uniquely
+    in the whole document so that we can select it to apply styles or manipulate 
+    using JavaScript. 
+
+    We can give only a single id name to each and every html element and it must
+    be unique in the whole web page.
+
+    We use id attribute to give id name. 
+
+    Ex : <p id = "para">It contains paragraph element</p>
+
+--> What is Class ? 
+    It is a group identifier.It is used to identify a single or group of elements.
+    We can give same class name to multiple html elements as well as multiple 
+    class names to the same html element.
+
+    We use class keyword to give class name. 
+
+    Ex : <div class = "container container-1">It is a container</div>
+         <div class = "container container-2">It is a container</div>
+         <div class = "container container-3">It is a container</div>
+         <div class = "container container-4">It is a container</div>
+         <div class = "container container-5">It is a container</div>
+
+*/
