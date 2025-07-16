aa12f9a4555e34f7f3a6d5b3fde8ea2532e1b405
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-07-16 18:17:54 +0530

    Attributes - Local Vs Global Vs Custom Attributes

---
+++ 2-HTML/1-HTML-Notes/10-Attributes.js
@@ -0,0 +1,26 @@
+/* 
+--------------------------------------- Attributes ---------------------------------
+
+--> What are Attributes ? 
+    Attributes are key value pairs that adds some behaviour to the html elements. 
+
+    1. Global Attributes : These are those attributes that can be used on every 
+    HTML element. 
+
+    Ex : contenteditable , contextmenu , hidden , style , class , id , draggable , 
+    lang , dir etc. 
+
+    2. Local Attributes : These are those attributes that are specific to a particular
+    html element. 
+
+    Ex  href , rel, enctype , type , placeholder , src etc. 
+
+    3. Custom Attributes : These are user-defined attributes that are used to 
+    store some data.They are not specified in HTML specifications but we create
+    them. 
+
+    <p coding="hello world">coding is fun</p>
+
+    It is fine to use them and it will work also. 
+
+*/
