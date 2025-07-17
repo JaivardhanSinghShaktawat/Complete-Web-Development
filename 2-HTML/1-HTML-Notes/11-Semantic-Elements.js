4add036d2b70556b52eebaf2c21783df689b23bc
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-07-17 21:59:41 +0530

    Semantic Elements , Core Web Vitals , Div Vs Span Element

---
+++ 2-HTML/1-HTML-Notes/11-Semantic-Elements.js
@@ -0,0 +1,51 @@
+/* 
+-------------------------------- Semantic Elements --------------------------------
+
+--> What are Semantic Elements ?  
+    Semantic Elements are HTML elements that provides some meaning.They provide
+    some meaning to the browser , screen readers and assistive technologies. 
+
+    Advantages Of Using Semantic Elements
+
+    1. It helps developers to understand the structure of the web page.
+    2. It provides some meaning which is helpful in SEO.
+    3. It helps screen readers , assistive technologies.
+
+--> Various types of semantic elements
+
+    header : it represents the introductory part of the web page. 
+    main : it represents the main content of the web page.
+    footer : it represents the bottom part of the web page. 
+    time : it represents time. 
+    details and summary : they are used to represent some content with details and
+    summary
+    aside : it represents the sidebar. 
+    article : it is used to represent independent content like blog post etc. 
+    nav : it represents navigation menu. 
+    address : it represents address. 
+    section : it specifies section in a document. 
+
+    There are many semantic elements.
+    Always use semantic elements.
+
+--> Div Vs Span Element 
+
+    Div : it is a generic block level element that group elements together.It stands
+    for division and it separates the content.
+
+    Span : it is a generic inline level element that is used to style a specific 
+    part of the text.
+
+    Note : Both div and span are not semantic elements.
+
+--> What is core web vitals ? 
+    It's an initiative by google that tells/calculates/determines the value that our website 
+    is providing.
+    It has three factors :-
+
+    1. loading speed
+    2. interactivity and responsiveness of the website.
+    3. commulative layout shift : It means that the content of the web page should
+    not get shift when CSS loads.
+
+*/
