bd93e0a8a18cf5057d286a2291217fbf7b390bd4
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-07-18 17:20:48 +0530

    HTML Iframe Element

---
+++ 2-HTML/1-HTML-Notes/13-Iframe-Element.js
@@ -0,0 +1,24 @@
+/* 
+-------------------------------- Iframe Element -------------------------------------
+
+--> Iframe Element : It is used to embed one document inside another document. 
+    It can also be used to embed pdf.
+
+    <iframe src="" height="" width="" allowfullscreen></iframe>
+
+    src : it stands for path or location of another document or pdf. 
+    height : height of the document.
+    width : width of the document. 
+
+    Iframe stands for inline frame.
+
+    We can embed wikipedia and other web pages/documents using iframe. 
+
+    srcdoc : it accepts html content. 
+    It has more priority than src attribute. 
+
+    <iframe srcdoc = "<h2>coding is fun</h2>"></iframe>
+
+    We can embed youtube videos , wikipedia web pages and even google maps.
+
+*/
