019be0123656baaa091963527692cf92619f0565
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-08-18 20:59:09 +0530

    Day 1 - Fundamentals Of Web And Internet

---
+++ 1-Fundamentals/1-Fundamentals.js
@@ -0,0 +1,57 @@
+/* 
+----------------------------------- Fundamentals -----------------------------------
+
+--> What is Client ?   
+    Client is generally the browser which makes request to the server.
+    Postman is another client that can also make request to the server.
+
+--> What is a Server ? 
+    It is a program that is connected to the internet , has IP address and is 
+    capable of sending responses and recieving incoming requests.
+
+--> What is Network ? 
+    Computers connected together via any medium is called as network.
+
+--> What is Internet ? 
+    It is a global network of network where multiple devices like laptop , 
+    computer etc are connected to each other via optic fibre cables across the
+    entire world.
+    Computers uses TCP/IP address to communicate with each other.
+
+--> What is IP address ? 
+    It is the address of the computer on the internet.
+
+--> What is MAC address ? 
+    It is the address of the hardware of the computer.
+
+--> What is URL ? 
+    It stands for Uniform Resource Locator.It is used to identify the resource
+    on the internet.
+
+--> What is Web ? 
+    It is part of the internet that allows us to share and access resources on the
+    internet.
+    It uses http protocol.
+
+--> What are resources ? 
+    Following are the resources :-
+
+    Audio 
+    Video 
+    CSS Files 
+    Fonts
+
+--> What is a Browser ? 
+    It is a C++ and JavaScript program that parse HTML , CSS and JavaScript and
+    renders them as web page.
+
+--> What are Protocols ? 
+    Set of rules are called as protocols.
+
+--> HTTP : It stands for HyperText Transfer Protocol.
+    HTTPS : It stands for HyperText Transfer Protocol Secure.
+    SMTP : it is used in mails.
+    SSH : it is used to transfer files over an unsecured network.
+    FTP : it is used to for file transfer.
+
+*/
---
+++ 1-Fundamentals/2-Frontend-Vs-Backend.js
@@ -0,0 +1,55 @@
+/* 
+------------------------------- Frontend Vs Backend -------------------------------
+
+--> Frontend : Anything that happens on the client (browser) side. 
+
+--> Backend : Anything that happens on the server side.
+
+--> Frontend Development : The development of the code that is executed on the 
+    client side is called as frontend development. 
+
+--> Backend Development : The development of the code that is executed on the 
+    server side is called as server development. 
+
+--> What are Static Websites ? 
+    These are those websites that are stored on the server and are send as it is 
+    without any transformation and changes to the client as the response.No new 
+    web page is created on the server , no data is fetched from the database.
+
+    Static websites do not change frequently.
+
+--> What are Dynamic Websites ? 
+    These are those websites whose data changes very fast.These are those websites
+    that are assembled and created on the server.A new web page is created on the
+    server , data is fetched from the database and injected into the web page.
+    For each and every request , a new web page is created on the server and than
+    it is send to the client as a response.
+
+--> What are Client Side Rendered Websites ? 
+    They are render on the client side. 
+    In CSR , a single html , css and javascript is send from the server to the 
+    client , after which javascript takes control over the whole application.
+    When user request for a new web page , than instead of making requests to the
+    server , javascript renders a different component.
+
+    All the Single Page Applications are called as Client Side Rendered Websites.
+
+--> What are Server Side Rendered Websites ? 
+    They are render on the server side.These websites are created and assembled
+    on the server.A new page is created on the server , data is fetched from the
+    database and injected into the web page and than the page is sent to the 
+    client as a response.
+
+--> How websites works ? 
+    User makes a request through url via browser to the server.The server than 
+    returns HTML , CSS and JavaScript Files.Browser parse these files and render
+    them as web page.
+    Behind the scenes ISP , DNS are also used to get the acutal IP address of the
+    server. 
+
+--> Why we map IP address with URL ? 
+    It's very difficult to remember or memorize the IP address of different
+    servers.That's why DNS maps them with url which is more human friendly and
+    easier to remember.
+
+*/
