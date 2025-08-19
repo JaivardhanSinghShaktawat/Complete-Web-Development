50e4873893d9e0a65411e8ed1388f91c0189fb09
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-08-19 15:50:38 +0530

    Day 2 - Fundamentals Of HTML

---
+++ 2-HTML/1-HTML-Notes/1-What-is-HTML.js
@@ -0,0 +1,69 @@
+/* 
+-------------------------------- What is HTML ? ------------------------------------
+
+--> What is HTML ? 
+    HTML stands for Hypertext Markup Language.
+    It's a markup language that is used to structure and describe the content 
+    of a webpage.
+    It is the basic building block of the web page.
+    It is the skeleton of the web page.
+
+    Syntax :  <openingTag>This is content </closingTag>
+
+--> HTML element = content + html tags (opening tag + closing tag)
+
+--> Opening tag : Name of the html tag wrapped in less than < and greater than >
+    symbol.
+
+    Closing tag : Name of the html tag wrapped in less than forward slash </ and 
+    greater than > symbol. 
+
+--> What is Markup Language ?   
+    It is some syntax that tells the browser how to structure and describe the content
+    of a web page.
+
+--> What is Live Server ?
+    It is a VS Code extensions that creates a local development server and host our
+    website on that server.Whenever the code changes , it refreshes the web page
+    automatically.It is used during development.
+
+--> What are Chrome Developer Tools ? 
+    Chrome Developer Tools are part of browser or browser runtime environment.They 
+    provide some tools like Element tab , Memory tab that are used during development 
+    and debugging.
+
+--> .html is the extension of html files.
+
+--> What is Hypertext ? 
+    It is the text which contains links to other text.This is the official 
+    definition that you can read by going to W3C website.
+    It is a text that contains links, when we click on this text/link than we 
+    redirect to a new web page or to some content on the same web page.
+
+--> How to run ?  : To run the HTML file , open the index.html file in any browser.
+
+--> We can create a dummy or local server using Live Server extension.
+
+--> Boilerplate code mans repetitive code. 
+
+--> HTML by default uses serif fonts.
+
+--> What is index.html File ? 
+    It is the first file that the client request to the server and server sends
+    to the client.
+    It is the root file or home page of our website.
+    Each and every website must have index.html file.
+
+--> What are Web Applications ? 
+    All the Client Side Rendered Websites or Single Page Applications are called
+    as Web Applications.
+
+--> What is Website ? 
+    All the Server Side Rendered Websites are called as websites.
+
+--> What are Comments ? 
+    Comments are used to document our code.It improves code readability.
+    It is ignored by compiler and interpreter and is not parsed and executed.
+
+    HTML Comments : <!--  --> 
+*/
