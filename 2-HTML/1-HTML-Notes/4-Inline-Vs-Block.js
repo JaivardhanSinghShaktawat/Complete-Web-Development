a2c21adaf50f0ab440ca9b7adaf235a0e597b820
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-08-20 19:26:50 +0530

    Day 3 - Inline Vs Block Elements

---
+++ 2-HTML/1-HTML-Notes/4-Inline-Vs-Block.js
@@ -0,0 +1,19 @@
+/* 
+---------------------------------- Inline Vs Block Elements ------------------------
+
+--> What are inline elements ? 
+    Inline elements takes only the required width according to their content.
+    They allow elements to come in their left , right , top and bottom. 
+    They don't take 100% width of their parent element. 
+
+    Ex : span , a , em , strong , img , a , q , s , del etc.
+
+--> What are block elements ? 
+    Block level elements takes 100% width of their parent element. 
+    They don't allow other elements to come in their left and right side.
+    They take 100% width of their parent elements even if there content is small.
+
+    Ex : div , section , footer , headings , p , pre , blockquote , figure , main , 
+    header , nav etc.
+
+*/
