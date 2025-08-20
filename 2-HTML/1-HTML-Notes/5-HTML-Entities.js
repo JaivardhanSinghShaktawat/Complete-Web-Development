241d3384836e18be8eb6c252df379630617adfe4
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-08-20 19:32:19 +0530

    Day 3 - HTML Entities

---
+++ 2-HTML/1-HTML-Notes/5-HTML-Entities.js
@@ -0,0 +1,26 @@
+/* 
+---------------------------------- HTML Entities -----------------------------------
+
+--> What are HTML Entites ? 
+    It allows us to add special characters in our document.
+
+    <pre style="font-size: 40px;">
+        &copy;
+        &reg; 
+        &pound; 
+        &dollar; 
+        &rarr; 
+        &larr; 
+        &uarr; 
+        &darr; 
+        &star; 
+        &trade; 
+        &times; 
+        &starf; 
+        &euro; 
+        &spades; 
+        &lt; 
+        &gt;
+    </pre>
+
+*/
