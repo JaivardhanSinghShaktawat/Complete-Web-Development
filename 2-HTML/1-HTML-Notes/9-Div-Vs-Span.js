e0b4c31d579dcf947de56490884309b5ff0dbd1c
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-08-21 20:35:00 +0530

    Day 4 - Div Vs Span Element

---
+++ 2-HTML/1-HTML-Notes/9-Div-Vs-Span.js
@@ -0,0 +1,10 @@
+/* 
+----------------------------------- Div Vs Span ------------------------------------
+
+--> Div : it is a generic container that is used to group/wrap elements together.
+    It stands for division.It is not a semantic element.
+
+--> Span : it is a generic inline element that is used to style a specic part of 
+    the text.
+
+*/
